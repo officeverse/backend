069b3022ee3ba39c691144cda8a8edf665dd8bcf
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2023-07-03 00:54:26 +0800

    update: serverless-s3-local

--- scripts/localS3PutObject.sh
+++ scripts/localS3PutObject.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # configure profile
 # aws configure --profile s3local
-aws --endpoint http://localhost:4569 s3 cp tmp/$1 s3://local-bucket/$1 --profile s3local
+aws --endpoint http://localhost:4569 s3 cp $1 s3://local-bucket/$1 --profile s3local
 # https://www.serverless.com/plugins/serverless-s3-local
